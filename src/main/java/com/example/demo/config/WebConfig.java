be6334517f94896c1e4d9d049cd66d2e852e64a0
yasu <yasusi0117@live.jp>
2022-02-03 13:18:49 +0900

    Interceptorを追加するために WebMvcConfigurerクラスを継承したWebConfigクラスを作成

---
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    @Qualifier("loggingInterceptor")
+    HandlerInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
